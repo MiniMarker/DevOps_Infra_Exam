875e8fd4fb197f32e37f43ed053faa04202f0b1d
examuser <exam@cmarker.no>
2018-11-15 10:39:27 +0100

    commit message

---
+++ concourse/java/task.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# we don't to anything with the artifact yet - we just want to build it.
+set -ueo pipefail
+
+export GREEN='\033[1;32m'
+export NC='\033[0m'
+export CHECK="√"
+export M2_LOCAL_REPO=".m2"
+
+# START Caching
+export ROOT_FOLDER=$( pwd )
+export REPO=repo
+
+M2_HOME="${HOME}/.m2"
+M2_CACHE="${ROOT_FOLDER}/maven"
+
+echo "Generating symbolic links for caches"
+
+[[ -d "${M2_CACHE}" && ! -d "${M2_HOME}" ]] && ln -s "${M2_CACHE}" "${M2_HOME}"
+# END Caching
+
+
+mvn -f source/pom.xml install 
+echo -e "${GREEN}${CHECK} Maven install${NC}"
---
+++ terraform/variables.tf
@@ -0,0 +1,8 @@
+
+variable "app_prefix" {
+  default = "devops-exam"
+}
+
+variable "pipeline_name" {
+  default = "devops-pipeline"
+}
